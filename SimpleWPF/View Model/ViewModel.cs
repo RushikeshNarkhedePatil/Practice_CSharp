ccf58d0bcdb134f7e71f2734626387491714b93c
RushikeshNarkhedePatil <rushikeshnarkhede4@gmail.com>
2022-11-03 17:57:17 +0530

    add

---
+++ SimpleWPF/View Model/ViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleWPF.View_Model
+{
+    class ViewModel
+    {
+    }
+}
---
+++ SimpleWPF/View/MainWin.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SimpleWPF.View
+{
+    /// <summary>
+    /// Interaction logic for MainWin.xaml
+    /// </summary>
+    public partial class MainWin : Window
+    {
+        public MainWin()
+        {
+            InitializeComponent();
+        }
+    }
+}
